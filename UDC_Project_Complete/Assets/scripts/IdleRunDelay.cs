014268f830b3cfed353bb1f20c9b3e0f66983bea
shn_tayal98 <sarthaktayal15@gmail.com>
2020-12-26 12:25:30 +0530

    animations imported

---
+++ UDC_Project_Complete/Assets/scripts/IdleRunDelay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleRunDelay : MonoBehaviour
+{
+    float updateTime = 0.0f;
+    static bool isTime3s = false;
+
+    //This Property returns the value of isTime3s private variable
+    public static bool IsTime { get{ return isTime3s; } }
+    
+    // FixedUpdate is called once per frame at 60 fps
+    void FixedUpdate()
+    {
+        updateTime += 0.02f;
+        if(updateTime >= 2.65f)
+        {
+            isTime3s = true;
+        }
+    }
+}
---
+++ UDC_Project_Complete/Assets/scripts/check02.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class check02 : MonoBehaviour
+{
+    float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<IdleRunDelay>();
+        transform.localRotation = Quaternion.identity;
+        transform.localRotation = Quaternion.Euler(0, 180, 0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (IdleRunDelay.IsTime && gameObject.transform.localRotation.y > 0)
+        {
+            Debug.Log("UDC_CHAR");
+            Debug.Log(Time.time);
+            transform.Rotate(0, -10, 0);
+        }
+    }
+}
+
+//
