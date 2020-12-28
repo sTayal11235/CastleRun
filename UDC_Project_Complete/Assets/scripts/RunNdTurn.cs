96fa02efccc041623f9c300dd5a93cafdc234c39
shn_tayal98 <sarthaktayal15@gmail.com>
2020-12-28 14:05:44 +0530

    script changes

--- UDC_Project_Complete/Assets/scripts/RunNdTurn.cs
+++ UDC_Project_Complete/Assets/scripts/RunNdTurn.cs
@@ -8,14 +8,20 @@ public class RunNdTurn : MonoBehaviour
     [SerializeField]
     Rigidbody myCharacter;
 
-    Vector3 runSpeed = new Vector3(0, -5.0f, 7.5f);
+    [SerializeField]
+    float speed;
+    Vector3 runSpeed;
 
     float rotationAngleY;
+    float coolDown = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
+        runSpeed = new Vector3(0, -5.0f, speed);
         gameObject.GetComponent<RunTimeKeeper>().enabled = false;
         myCharacter = GetComponent<Rigidbody>();
+        gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
         rotationAngleY = 0.0f;
     }
 
@@ -29,6 +35,7 @@ public class RunNdTurn : MonoBehaviour
             myCharacter.velocity = new Vector3(0, 0, 0);
             rotationAngleY += 90.0f;
             myCharacter.rotation = Quaternion.Euler(0, rotationAngleY, 0);
+            Debug.Log(myCharacter.rotation.eulerAngles.y);
             this.FixedUpdate();
         }
 
@@ -37,13 +44,49 @@ public class RunNdTurn : MonoBehaviour
             myCharacter.velocity = new Vector3(0, 0, 0);
             rotationAngleY -= 90.0f;
             myCharacter.rotation = Quaternion.Euler(0, rotationAngleY, 0);
+            Debug.Log(myCharacter.rotation.eulerAngles.y);
             this.FixedUpdate();
         }
 
+        if(Input.GetKeyDown(KeyCode.Space) && coolDown >= 1.0f)
+        {
+            JumpUp();
+            coolDown = 0.0f;
+        }
+
+        coolDown += Time.deltaTime;
     }
 
     void FixedUpdate()
     {
+        if(myCharacter.rotation.eulerAngles.y >= 85 && myCharacter.rotation.eulerAngles.y < 95)
+        {
+            runSpeed = new Vector3(speed, - 5.0f, 0.0f);            
+            Debug.Log("X");
+        }
+
+        else if(myCharacter.rotation.eulerAngles.y >= 175 && myCharacter.rotation.eulerAngles.y < 185)
+        {
+            runSpeed = new Vector3(0.0f, -5.0f, -1 * speed);
+            Debug.Log("-Z");
+        }
+        else if(myCharacter.rotation.eulerAngles.y >= 265 && myCharacter.rotation.eulerAngles.y < 275)
+        {
+            runSpeed = new Vector3(-1 * speed, -5.0f, 0.0f);
+            Debug.Log("-X");
+        }
+        else
+        {
+            runSpeed = new Vector3(0.0f, -5.0f, speed);
+            Debug.Log("Z");
+        }
+
         myCharacter.velocity = runSpeed;
+    }
+
+    void JumpUp()
+    {
+        Vector3 liftUp = new Vector3(runSpeed.x * 10, 200.0f, runSpeed.z * 10);
+        myCharacter.AddForce(liftUp, ForceMode.Impulse);
     }
 }
