5a5090026f3f6d8b28da19ef58971a44fc69d8ab
shn_tayal98 <sarthaktayal15@gmail.com>
2020-12-29 20:56:22 +0530

    jump correction

--- UDC_Project_Complete/Assets/scripts/RunNdTurn.cs
+++ UDC_Project_Complete/Assets/scripts/RunNdTurn.cs
@@ -10,10 +10,14 @@ public class RunNdTurn : MonoBehaviour
 
     [SerializeField]
     float speed;
+
     Vector3 runSpeed;
 
     float rotationAngleY;
-    float coolDown = 0.0f;
+    float noJumpDuration = 0.0f;
+
+    [SerializeField]
+    float jumpVel;
 
     // Start is called before the first frame update
     void Start()
@@ -46,45 +50,50 @@ public class RunNdTurn : MonoBehaviour
             this.FixedUpdate();
         }
 
-        if(Input.GetKeyDown(KeyCode.Space) && coolDown >= 1.0f)
+        if(Input.GetKeyDown(KeyCode.Space) && noJumpDuration >= 0.65f)
         {
-            JumpUp();
-            coolDown = 0.0f;
+            IdleRunDelay.PlayerJumped();
+            Jump();
+            noJumpDuration = 0.0f;
         }
 
-        coolDown += Time.deltaTime;
+        noJumpDuration += Time.deltaTime;
     }
 
     void FixedUpdate()
     {
-        if(myCharacter.rotation.eulerAngles.y >= 85 && myCharacter.rotation.eulerAngles.y < 95)
+        if(!IdleRunDelay.JumpedTimerStarted && IdleRunDelay.JumpedTimerEnded)
         {
-            runSpeed = new Vector3(speed, - 8.0f, 0.0f);            
-            Debug.Log("X");
-        }
+            if (myCharacter.rotation.eulerAngles.y >= 85 && myCharacter.rotation.eulerAngles.y < 95)
+            {
+                runSpeed = new Vector3(speed, -1 * speed, 0.0f);
+                Debug.Log("X");
+            }
 
-        else if(myCharacter.rotation.eulerAngles.y >= 175 && myCharacter.rotation.eulerAngles.y < 185)
-        {
-            runSpeed = new Vector3(0.0f, -8.0f, -1 * speed);
-            Debug.Log("-Z");
-        }
-        else if(myCharacter.rotation.eulerAngles.y >= 265 && myCharacter.rotation.eulerAngles.y < 275)
-        {
-            runSpeed = new Vector3(-1 * speed, -8.0f, 0.0f);
-            Debug.Log("-X");
-        }
-        else
-        {
-            runSpeed = new Vector3(0.0f, -8.0f, speed);
-            Debug.Log("Z");
-        }
+            else if (myCharacter.rotation.eulerAngles.y >= 175 && myCharacter.rotation.eulerAngles.y < 185)
+            {
+                runSpeed = new Vector3(0.0f, -1 * speed, -1 * speed);
+                Debug.Log("-Z");
+            }
+            else if (myCharacter.rotation.eulerAngles.y >= 265 && myCharacter.rotation.eulerAngles.y < 275)
+            {
+                runSpeed = new Vector3(-1 * speed, -1 * speed, 0.0f);
+                Debug.Log("-X");
+            }
+            else
+            {
+                runSpeed = new Vector3(0.0f, -1 * speed, speed);
+                Debug.Log("Z");
+            }
 
-        myCharacter.velocity = runSpeed;
+            myCharacter.velocity = runSpeed;
+        }
     }
 
-    void JumpUp()
+    void Jump()
     {
-        Vector3 liftUp = new Vector3(runSpeed.x * 10, 200.0f, runSpeed.z * 10);
-        myCharacter.AddForce(liftUp, ForceMode.Impulse);
+        float runX = runSpeed.x;
+        float runZ = runSpeed.z;
+        myCharacter.velocity = new Vector3(runX * 0.5f, jumpVel, runZ * 0.5f);
     }
 }
--- UDC_Project_Complete/Assets/scripts/Utility/IdleRunDelay.cs
+++ UDC_Project_Complete/Assets/scripts/Utility/IdleRunDelay.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class IdleRunDelay : MonoBehaviour
 {
     float updateTime = 0.0f;
+    float jumpTimer = 0.0f;
     static bool isTime3s = false;
     static bool cameraSwap = false;
     static bool shouldRun = false;
-
+    static bool jumpedTimerStarted = false;
+    static bool jumpedTimerEnded = true;
     //This Property returns the value of isTime3s private variable
     public static bool IsTime { get{ return isTime3s; } }
 
     public static bool CameraSwapTime { get { return cameraSwap; } }
     
     public static bool ShouldRun { get { return shouldRun; } }
-    
+
+    public static bool JumpedTimerStarted { get { return jumpedTimerStarted; } set { jumpedTimerStarted = value; } }
+
+    public static bool JumpedTimerEnded { get { return jumpedTimerEnded; } set { jumpedTimerEnded = value; } }
+
+
     // FixedUpdate is called once per frame at 60 fps
     void FixedUpdate()
     {
@@ -34,5 +41,25 @@ public class IdleRunDelay : MonoBehaviour
         {
             shouldRun = true;
         }
+
+        if(jumpedTimerStarted && !jumpedTimerEnded)
+        {
+            if(jumpTimer <= 0.5f)
+            {
+                jumpTimer += 0.02f;
+            }
+            else
+            {
+                jumpedTimerStarted = false;
+                jumpedTimerEnded = true;
+                jumpTimer = 0.0f;
+            }
+        }
+    }
+
+    public static void PlayerJumped()
+    {
+        jumpedTimerStarted = true;
+        jumpedTimerEnded = false;
     }
 }
